4762cb376c205bf426124a880d4f4e7ec262833e
henryrosalesmendez <hrosmendez@gmail.com>
2024-01-24 11:42:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.header('header example 2')
 streamlit.text('text example 3')
 streamlit.text('text example 4')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
